064ecbdff40cea47ed55563fb222537945adf929
djbp17 <daven.piloton17@gmail.com>
2024-10-28 17:39:47 +0800

    Added additional file

---
+++ app/about/page.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About JayCodes</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae quisquam
+        consectetur debitis atque non id fugiat vitae deserunt eos magnam nemo,
+        nesciunt delectus eius iure impedit voluptas natus, ullam cumque?
+      </p>
+    </div>
+  );
+};
+
+export default AboutPage;
--- app/page.js
+++ app/page.js
@@ -6,7 +6,13 @@ const Homepage = () => {
       <h1>Welcome to JayCodes</h1>
       <ul>
         <li>
-          <Link></Link>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/about">About</Link>
+        </li>
+        <li>
+          <Link href="/about/team">Team</Link>
         </li>
       </ul>
     </div>
